02aa43401ed2cc6a2674dcd60587ee847e457a68
amirpma <amir.pma1378@gmail.com>
2020-11-01 23:48:59 +0330

    fixed spelling

--- src/main/compileErrorException/nameErrors/ClassInCyclicInheritance.java
+++ src/main/compileErrorException/nameErrors/ClassInCyclicInheritance.java
@@ -7,7 +7,7 @@ import main.compileErrorException.CompileErrorException;
 public class ClassInCyclicInheritance extends CompileErrorException {
 
     public ClassInCyclicInheritance(ClassDeclaration classDeclaration) {
-        super(classDeclaration.getLine(), "Class " + classDeclaration.getClassName().getName() + " is in a inheritance cycle");
+        super(classDeclaration.getLine(), "Class " + classDeclaration.getClassName().getName() + " is in an inheritance cycle");
     }
 
 }
